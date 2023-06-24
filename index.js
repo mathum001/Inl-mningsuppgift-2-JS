51126d9f163c5c925e8f507f4e2238de6787f431
mathum001 <mattas.hummer@hotmail.com>
2023-06-24 05:26:34 +0200

    Added date

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ let intervalID;
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
+const date = document.getElementById("date");
+
+// Initilize, börjar med vädret i London automatiskt
+setInterval((getCityCoords(city)), 1800000);
 
 
-getCityCoords(city);
-testBtn[0].addEventListener("click", () =>{
+testBtn[0].addEventListener("click", () => {
     getCityCoords(testSearch[0].value);
     clearInterval(intervalID);
 })
@@ -35,6 +38,7 @@ function getCityCoords(name) {
 
 }
 
+//Fetchar vädret mha koordinaterna
 function getWheather(lat, lon) {
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=b823b4873417423ca400dc5b452e442c&units=metric').then(
         function (response) {
@@ -45,10 +49,8 @@ function getWheather(lat, lon) {
             response.json().then(function (data) {
                 console.log(data);
                 displayWeather(data);
-                /* clearInterval(intervalID); */
                 intervalID = setInterval(clock, 1000, data.timezone);
-                /* setInterval(clock, 1000, data.timezone); */
-                
+
             });
         }
     )
@@ -57,44 +59,44 @@ function getWheather(lat, lon) {
         })
 }
 
-
- function displayWeather(data){
+////Displays weather
+function displayWeather(data) {
     document.getElementById("bottom").innerHTML = "";
     let objekt = document.getElementById("bottom");
 
-    let huvudValue = document.createElement("h1");
+    let stadNamn = document.createElement("h1");
+    stadNamn.innerHTML = data.name;
+    let huvudValue = document.createElement("h2");
     huvudValue.innerHTML = data.weather[0].main;
     let bild = document.createElement("IMG");
     bild.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
-    let stadNamn = document.createElement("h2");
-    stadNamn.innerHTML = data.name;
     let grader = document.createElement("h3");
-    grader.innerHTML = "Temperatur: " + data.main.temp + "°C";
+    grader.innerHTML = "Temperature: " + data.main.temp + "°C";
     let luftTryck = document.createElement("h3");
-    luftTryck.innerHTML = "Lufttryck: " + data.main.pressure + "hPa";
+    luftTryck.innerHTML = "Pressure: " + data.main.pressure + "hPa";
     let fuktighet = document.createElement("h3");
-    fuktighet.innerHTML = "Fuktighet: " + data.main.humidity + "%";
+    fuktighet.innerHTML = "Humidity: " + data.main.humidity + "%";
     let vind = document.createElement("h3");
     vind.innerHTML = "Wind: " + data.wind.speed + "m/s";
 
-    
-    objekt.append(huvudValue, bild, stadNamn, grader, luftTryck, fuktighet, vind);
-    
-    /* return (data.timezone); */
+
+    objekt.append(stadNamn, huvudValue, bild, grader, luftTryck, fuktighet, vind);
+
+
 
 
 }
 
-/////////Clock below///////
+/////////Clock function///////
 
-function clock(offSet){
-    
+function clock(offSet) {
+
     let localTime = new Date();
     let tidZonen = localTime.getTimezoneOffset();
-    if(offSet !== 0){
-        hours.innerHTML = localTime.getHours() + (tidZonen/60) + (offSet/3600);
+    if (offSet !== 0) {
+        hours.innerHTML = localTime.getHours() + (tidZonen / 60) + (offSet / 3600);
     }
-    else{
+    else {
         hours.innerHTML = localTime.getHours();
     }
     minutes.innerHTML = localTime.getMinutes();
@@ -102,15 +104,15 @@ function clock(offSet){
 
     let spans = document.querySelectorAll('span');///Adds a 0 if the number is < 10
     spans.forEach((span) => {
-        if(span.innerHTML.length === 1){
-        span.innerHTML = `0${span.innerHTML}`;
-        }})
-    
-   /*  console.log(tidZonen + "   tidszon");
-    console.log(offSet + "   offset");
-    console.log(localTime); */
+        if (span.innerHTML.length === 1) {
+            span.innerHTML = `0${span.innerHTML}`;
+        }
+    })
 
-    
+    date.innerHTML = (localTime.getDate()+ "/" + localTime.getMonth() +"/"+localTime.getFullYear() );
+
+
+
 }
 
 
